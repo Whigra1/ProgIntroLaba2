fc3445c2c5326d30e17de9f0df3cfc447cc83fc0
nazarevitsch <45229160+nazarevitsch@users.noreply.github.com>
2019-03-20 08:56:23 +0200

    Final ls!

---
+++ Main.java
@@ -0,0 +1,26 @@
+package laba2;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        String str;
+        if (args.length == 0)
+            str = new Scanner(System.in).nextLine();
+        else
+            str = args[0];
+        QueueArray<String> tokens = ShuntingYard.toPostfix(str);
+        StackArray<String> s = new StackArray<>(tokens.getSize());
+        while (!tokens.isEmpty()) {
+            String elem = tokens.pop();
+            if (StringParser.isDigit(elem))
+                s.push(elem);
+            else {
+                double a = Double.parseDouble(s.pop());
+                double b = Double.parseDouble(s.pop());
+                s.push(String.valueOf(Operators.calculate(b, a, elem)));
+            }
+        }
+        System.out.println(s.peek());
+    }
+}
